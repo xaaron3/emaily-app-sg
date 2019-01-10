bb06eb6acbb0798702da21fe668d305e6eb39bbe
Aaron <39026927+xaaron3@users.noreply.github.com>
2019-01-10 17:24:02 -0500

    github add

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./services/passport');
+require('./models/User');
+
+// use mongoose to create new collection in mongo called "users"
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
