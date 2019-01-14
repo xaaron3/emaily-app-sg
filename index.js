eed8ba8848f3d654c82cd9aa240bcf63b044e275
Aaron <39026927+xaaron3@users.noreply.github.com>
2019-01-14 07:44:15 -0500

    setting pck.json scripts for prod

--- index.js
+++ index.js
@@ -26,5 +26,17 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+   // express will serve up prod assets, like main.js or main.css files
+   app.use(express.static('client/build'));
+
+   // express will serve up the index.html file
+   // if it doesnt recognize the route
+   const path = require('path');
+   app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+   });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
