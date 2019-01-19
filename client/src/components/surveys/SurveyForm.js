5a894ae75c8698155bc79db0842587a1c896ba5b
aaron <xakong@outlook.com>
2019-01-19 04:08:56 -0500

    displaying invalid emails, toggle visibility, advancing from surveyform, retreating, persisting form values, restore form fields, dump values, redirect submit

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -4,17 +4,12 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
-
-const FIELDS = [
-   { label: 'Survey Title', name: 'title' },
-   { label: 'Subject Line', name: 'subject' },
-   { label: 'Email Body', name: 'body'},
-   { label: 'Recipient List', name: 'emails'}
-];
+import validateEmails from '../../utilities/validateEmails';
+import formFields from './formFields';
 
 class SurveyForm extends Component {
    renderFields() {
-      return _.map(FIELDS, ({ label, name }) => {
+      return _.map(formFields, ({ label, name }) => {
          return (
             <Field key={name} component={SurveyField} type="text" label={label} name={name} />
          );
@@ -24,7 +19,7 @@ class SurveyForm extends Component {
    render() {
       return (
          <div>
-            <form onSubmit={this.props.handleSubmit((values) => console.log(values))}>
+            <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
                {this.renderFields()}
                <Link to="/surveys" className="red btn-flat white-text">
                   Cancel
@@ -42,17 +37,19 @@ class SurveyForm extends Component {
 function validate(values) {
    const errors = {};
 
-   _.each(FIELDS, ({ name }) => {
+   errors.recipients = validateEmails(values.recipients || '');
+
+   _.each(formFields, ({ name }) => {
       if (!values[name]) {
          errors[name] = 'You must provide a value';
       }
    })
-
 
    return errors;
 }
 
 export default reduxForm({
    validate: validate,
-   form: 'surveyForm'
+   form: 'surveyForm',
+   destroyOnUnmount: false
 })(SurveyForm);
--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,16 +1,32 @@
 // surveyNew shows surveyForm and SurveyFormReview
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends Component {
+   state = { showFormReview: false };
+
+   renderContent() {
+      if (this.state.showFormReview === true) {
+         return <SurveyFormReview
+                  onCancel={() => this.setState({ showFormReview: false })} />
+      } else {
+         return <SurveyForm 
+                  onSurveySubmit={() => this.setState({ showFormReview: true })} />
+      }
+   }
+
    render() {
       return (
          <div>
             SurveyNew!
-            <SurveyForm />
+            {this.renderContent()}
          </div> 
       )
    }
 }
 
-export default SurveyNew;
+export default reduxForm({
+   form: 'surveyForm'
+})(SurveyNew);
