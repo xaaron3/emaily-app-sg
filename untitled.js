a9dc4925e04bcb605d0a6e9bc9b72a11a86b1bcb
aaron <xakong@outlook.com>
2019-01-20 05:04:08 -0500

    fetching surv list, rendering surveys w redux react, reversing order, adding materialize card

--- untitled.js
+++
@@ -1,9 +0,0 @@
-Survey.updateOne({
-   id: surveyId,
-   recipients: {
-      $elemMatch: { email: email, responded: false }
-   }
-}, {
-   $inc: { [choice]: 1 },
-   $set: { 'recipients.$.responded': true }
-})
